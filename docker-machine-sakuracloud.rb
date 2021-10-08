31f66833bd6023d6f4f8bb5dfa23c35108c3e545
Sacloud <sacloud.users@gmail.com>
2021-10-08 15:00:29 +0900

    Brew formula update for docker-machine-sakuracloud version v1.6.0

--- docker-machine-sakuracloud.rb
+++ docker-machine-sakuracloud.rb
@@ -5,29 +5,34 @@
 class DockerMachineSakuracloud < Formula
   desc ""
   homepage "https://github.com/sacloud/docker-machine-sakuracloud"
-  version "1.5.0"
+  version "1.6.0"
   license "Apache 2.0"
   bottle :unneeded
 
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/sacloud/docker-machine-sakuracloud/releases/download/v1.5.0/docker-machine-driver-sakuracloud_1.5.0_darwin_amd64.zip"
-    sha256 "eae4c955bcf3b0dc3901e4a4162d190d086232cff9ffe64036b6a0c0339beb02"
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/sacloud/docker-machine-sakuracloud/releases/download/v1.6.0/docker-machine-driver-sakuracloud_1.6.0_darwin_amd64.zip"
+      sha256 "ea537a43e15885113189638e3a2be50c783dbc7f161585f14485e009718f5808"
+    end
+    if Hardware::CPU.arm?
+      url "https://github.com/sacloud/docker-machine-sakuracloud/releases/download/v1.6.0/docker-machine-driver-sakuracloud_1.6.0_darwin_arm64.zip"
+      sha256 "428ccecf30294cdb0e1ef22870365fdb2761896814aa73c5070f1ba562610e4b"
+    end
   end
-  if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/sacloud/docker-machine-sakuracloud/releases/download/v1.5.0/docker-machine-driver-sakuracloud_1.5.0_darwin_arm64.zip"
-    sha256 "4291e937189142849c8a3935f06500f27dc29f04edab475ecfb3588ed8d13c65"
-  end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/sacloud/docker-machine-sakuracloud/releases/download/v1.5.0/docker-machine-driver-sakuracloud_1.5.0_linux_amd64.zip"
-    sha256 "3326a2a3d8674082091cbe3d1c902fdd94ec86fe55dbd9371f72f16c31062055"
-  end
-  if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/sacloud/docker-machine-sakuracloud/releases/download/v1.5.0/docker-machine-driver-sakuracloud_1.5.0_linux_arm.zip"
-    sha256 "54f3e6fc6e1ef3545717c3088e9e710e03b7b68a2b91f5beb19c8b2b4329760e"
-  end
-  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/sacloud/docker-machine-sakuracloud/releases/download/v1.5.0/docker-machine-driver-sakuracloud_1.5.0_linux_arm64.zip"
-    sha256 "8aa87b1a38c3c02da4f0d2bed4b868ab668a2f1de2e153462e1fa63eaa6fa546"
+
+  on_linux do
+    if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
+      url "https://github.com/sacloud/docker-machine-sakuracloud/releases/download/v1.6.0/docker-machine-driver-sakuracloud_1.6.0_linux_arm.zip"
+      sha256 "ec4a4fd952b59fc7da8c3b7d97b0dcc8b487d520965e327ea855c8ec1190938a"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/sacloud/docker-machine-sakuracloud/releases/download/v1.6.0/docker-machine-driver-sakuracloud_1.6.0_linux_amd64.zip"
+      sha256 "610f028b47dbd5d484863f8e8789f7573a992d6b7e5421a243c76857aabefc89"
+    end
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/sacloud/docker-machine-sakuracloud/releases/download/v1.6.0/docker-machine-driver-sakuracloud_1.6.0_linux_arm64.zip"
+      sha256 "bc44b36f17c4fef636807eea1697625f30d11a6b8ddece80f7df25b6a8bf05e6"
+    end
   end
 
   def install
